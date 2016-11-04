9367da530a7703c544804b6c0cc0ee0323fe26ab
jpjanet <jon.paul.janet@gmail.com>
2016-11-04 10:42:55 -0400

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 ## any neccessary shell operations, not needed at this stage
-#alias_path=$PREFIX
-echo "$PREFIX"
-#cat 
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt

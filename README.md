62e8e97eadae985b25d0ad0c73e39c7b054d7eb0
jpjanet <jpjanet@mit.edu>
2018-10-13 14:19:52 -0400

    update conda yaml file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # molSimplifyConda
 Conda repo for installing molSimplify
+
+This set of scripts will download the
+lastest build information from the most
+recent git version
--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+
 ## any neccessary shell operations, not needed at this stage
+## normal build, log build info into record.txt
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt

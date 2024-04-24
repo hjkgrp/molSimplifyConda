d94d91fde511ceb1c4b0036b775ad056fbfc5acc
Gianmarco <gianmarcogterrones@gmail.com>
2024-04-24 14:59:48 -0400

    removed flags from build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
-
-
-## any neccessary shell operations, not needed at this stage
-## normal build, log build info into record.txt
-$PYTHON -m pip install . --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install .

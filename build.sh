548573542566ab2708085f1961c6353e5e04f90c
Gianmarco <gianmarcogterrones@gmail.com>
2024-04-24 14:41:58 -0400

    replaced setup.py commands

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 
 ## any neccessary shell operations, not needed at this stage
 ## normal build, log build info into record.txt
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . --single-version-externally-managed --record=record.txt
